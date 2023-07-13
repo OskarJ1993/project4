f8417b1cfa121d4fc4469beec456a6603811805a
Oskar Johansson <92440552+OskarJ1993@users.noreply.github.com>
2023-07-13 21:45:42 +0200

    fix server issues

--- project4/settings.py
+++ project4/settings.py
@@ -19,7 +19,7 @@ environ.Env.read_env()
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
